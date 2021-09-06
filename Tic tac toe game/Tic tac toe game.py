ecaabb8bdebaad87e6d3a0e5ad3780684ddedcaa
Jovan Kueh <jovan.kueh@gmail.com>
2021-09-06 11:17:33 -0400

    update tic tac toe

--- Tic tac toe game/Tic tac toe game.py
+++ Tic tac toe game/Tic tac toe game.py
@@ -10,11 +10,18 @@ def printBoard(board):
     print(' {} | {} | {} '.format(board['7'], board['8'], board['9']))
 
 printBoard(theBoard)
+for turn in range(1, 10):
+    # If turn is even number then the character is circle.
+    # If turn is odd number then the character is x.
+    if (turn % 2) == 0:
+        character = '0'
+    else:
+        character = 'X'
 
-userInput = input("input a number from 1 to 9.")
-print(userInput)
+    userInput = input("input a number from 1 to 9.")
+    theBoard[userInput] = character
+    printBoard(theBoard)
 
-currentTurn = 'X'
-
-theBoard[userInput] = "x"
-printBoard(theBoard)
+# bugs
+# 1. Cannot replace existing characters
+# 2. When get 3 in a row, gets win and stops game.
